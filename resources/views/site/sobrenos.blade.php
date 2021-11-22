c0c77242dbf9e362ee95e7a6f861d61f6703e2a4
carmo <gabriel.carmo@devcodes.com.br>
2021-11-22 17:45:49 -0300

    Adição de rotas

---
+++ resources/views/site/sobrenos.blade.php
@@ -0,0 +1,20 @@
+<h2>Sobre Nós</h3>
+
+<ul>
+    <li>
+        <a href="{{route('site.index')}}">Principal</a>
+    </li>
+    
+    <li>
+        <a href="{{route('site.sobrenos')}}">Sobre</a>
+    </li>
+    
+    <li>
+        <a href="{{route('site.contato')}}">Contato</a>
+    </li>
+    
+    <li>
+        <a href="{{route('app.login')}}">Login Sistema</a>
+    </li>
+
+</ul>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,26 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', 'MainController@principal')->name('site.index');
+
+Route::get('/sobre-nos', 'AboutController@sobrenos')->name('site.sobrenos');
+
+Route::get('/contato', 'ContactController@contato')->name('site.contato');
+
+Route::get('/login', function(){return 'login';})->name('site.login');
+
+Route::prefix('/app')->group(function(){
+    Route::get('/login', function(){return 'Login interno';})->name('app.login');
+    Route::get('/clientes', function(){return 'clientes';})->name('app.clientes');
+    Route::get('/fornecedores', function(){return 'fornecedores';})->name('app.fornecedores');
+    Route::get('/produtos', function(){return 'produtos';})->name('app.produtos');
 });
+
+Route::get('/logout', function(){
+    
+    return redirect()->route('site.volte');
+    //Route::redirect('logout', 'voltesempre');
+
+})->name('site.logout');
+
+Route::get('/voltesempre', function(){return 'Volte sempre!';})->name('site.volte');
